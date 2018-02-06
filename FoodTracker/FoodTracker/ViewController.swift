9cc4148849a6c87ba053fd430f1a878fa57a8144
Alexa <willi499@miamioh.edu>
2018-02-06 00:03:12 -0500

    setting up repository again

---
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Alexa Williams on 12/29/17.
+//  Copyright © 2017 Alexa Williams. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    //MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    //MARK: Actions
+    
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        mealNameLabel.text = "Default Text"
+    }
+}
+
---
+++ Workspace/Testing/src/Test.java
@@ -0,0 +1,15 @@
+
+public class Test {
+	public static void main(String[] args) {
+		int i = Integer.parseInt("34");
+		int t = Integer.parseInt("3");
+		
+		String z = "33";
+		String k = "3";
+		
+		System.out.println(z + k);
+		
+		
+		
+	}
+}
